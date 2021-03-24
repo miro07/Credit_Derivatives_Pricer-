eb2224f3ea8e136ad5df7e4e652861ab6f6e7751
Marwan.Jabote <47955911+miro07@users.noreply.github.com>
2021-03-24 22:00:18 +0000

    Update CDO.py

--- Modules/CDO.py
+++ Modules/CDO.py
@@ -7,8 +7,6 @@ from statistics import mean
 
 
 class CDO_Tranche():
-    """Pays off 0 if less than k defaults occur, pays l-k if more than l defaults
-    occur, and pays (x-k) if k <= x < l defaults occur"""
     def __init__(self,portfolio,MNCDS,s,k,maturity,rate = 0.01):
         #super(KthToLthTranche, self).__init__(DiscountCurve)
         self.portfolio = portfolio
@@ -22,8 +20,8 @@ class CDO_Tranche():
         assert  k < k+s , 'The upper of the tranche can not be below the lower of it '
 
     def CDO_Pricing(self,default_times):
-        """Pays off 0 if less than k defaults occur, pays l-k if more than l defaults
-    occur, and pays (x-k) if k <= x < l defaults occur"""
+        """Pays off 0 if less than lower bond  defaults occur, pays upper-lower if more than upper defaults
+    occur, and pays (x-lower bond) if lower bond <= x < upper bond defaults occur"""
         defaults_before_t = sum(map(lambda x: x < self.Maturity, default_times))
         if defaults_before_t > (self.upper * len(self.MNCDS.CDSs)):
             value = (self.upper - self.lower)
